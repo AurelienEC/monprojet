ef8e795276f70c61af45e70446a6292c0615691f
Aurelien <aurelien.antonio@gmail.com>
2019-12-18 05:58:36 -0500

    Initial Commit

---
+++ monprojet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  monprojet
+//
+//  Created by Aurélien Antonio on 18/12/2019.
+//  Copyright © 2019 Aurélien Antonio. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
